4a3e600c5b3116dbe3c0b4119819dc6551b1d66e
MuscleLord <hoggren.tomas@gmail.com>
2022-02-12 18:19:23 +0100

    updated

--- serverless/get_weather.js
+++ serverless/get_weather.js
@@ -6,8 +6,9 @@ exports.handler = async (event, context) => {
 	const params = JSON.parse(event.body);
 	const { lat, lon, units } = params;
 	const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=${units}&lang=sv&exclude=minutely,hourly,alerts&appid=${API_KEY}`;
+	const encodedUrl = encodeURI(url);
 	try {
-		const weatherStream = await fetch(url);
+		const weatherStream = await fetch(encodedUrl);
 		const weatherJson = await weatherStream.json();
 		return {
 			statusCode: 200,
