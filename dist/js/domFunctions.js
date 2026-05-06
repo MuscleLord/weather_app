3c5934d9357599484002d0596482f7ec97bb90da
Tomas Höggren <hoggren.tomas@gmail.com>
2026-05-07 01:04:56 +0200

    Update domFunctions.js

--- dist/js/domFunctions.js
+++ dist/js/domFunctions.js
@@ -305,9 +305,12 @@ const displayCurrentConditions = (currentConditionsArray) => {
 
 const displaySixDayForecast = (weatherJson) => {
 	if (!weatherJson.list) return;
-	for (let i = 1; i <= 6; i++) {
-		const dfArray = creatDailyForecastDivs(weatherJson.list[i]);
-		displayDailyForecast(dfArray);
+	
+	for (let i = 1; i <= 40; i++) {
+		if(weatherJsop.list[i].dt_txt.includes("12:00:00"){
+			const dfArray = creatDailyForecastDivs(weatherJson.list[i]);
+			displayDailyForecast(dfArray);
+		}
 	}
 };
 
